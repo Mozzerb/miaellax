d729d9b32a9fbcd668a75f711ce1e10162c7f062
Mozzerb <36933596+Mozzerb@users.noreply.github.com>
2020-09-21 11:47:10 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 60) {
-        $('.navbar').css('background','#FFFFFF');
-    } else {
-        $('.navbar').css('background','transparent');
-    }
-});
-
